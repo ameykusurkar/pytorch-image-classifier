8af7125549e22d793d1a3c13eb560b378d86b45c
Amey Kusurkar <amey.kusurkar14@imperial.ac.uk>
2018-02-02 18:01:51 +0000

    Load and process MNIST dataset

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+BATCH_SIZE = 10
+
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+
+trainset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)

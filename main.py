43a18de3a98686d1c403c4869823fc468ba8ac7a
Amey Kusurkar <amey.kusurkar14@imperial.ac.uk>
2018-02-03 18:12:13 +0000

    Implement model and forward function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
 BATCH_SIZE = 10
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
@@ -11,3 +15,23 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuff
 
 testset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(500, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 500)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+model = Net()
+output = model(Variable(torch.randn(1, 1, 32, 32)))
+print(output)
